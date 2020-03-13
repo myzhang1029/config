3733ad9af05303dccbb6999f57c2a87d298bd5aa
Zhang Maiyun <myzhang1029@hotmail.com>
2020-03-13 12:48:58 +0800

    Advance use.sh

--- use.sh
+++ use.sh
@@ -1,29 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 OLDWORKDIR="$(pwd)"
 BASE="$(dirname $0)"
 FULLPATH="$(realpath $BASE)"
-
+IGNORE="use.sh README.md"
 cd "$BASE"
 
-for i in *
-do
-    if [ "$i" = "use.sh" ]
+backup_one() {
+    echo "Moving ~/.$1 to backup/$1"
+    mkdir -p "$OLDWORKDIR/backup"
+    mv "$HOME/.$1" "$OLDWORKDIR/backup/$1"
+}
+
+do_link() {
+    if [ -e "$HOME/.$1" ]
     then
-        continue
-    fi
-    if [ -e "$HOME/.$i" ]
-    then
-        echo "~/.$i exists, overwrite? [y/n]"
+        echo "~/.$1 exists, overwrite? [y/n]"
         read -r tf
         if [ "$tf" = "y" ]
         then
-            echo "Moving ~/.$i to backup/$i"
-            mkdir -p "$OLDWORKDIR/backup"
-            mv "$HOME/.$i" "$OLDWORKDIR/backup/$i"
+            backup_one $1
         else
             echo "Not overriding"
             continue
         fi
     fi
-    ln -s "$FULLPATH/$i" "$HOME/.$i"
+    ln -s "$FULLPATH/$1" "$HOME/.$1"
+}
+
+included() {
+    for en in $IGNORE
+    do
+        if [ "$1" = "$en" ]
+        then
+            return 1
+        fi
+    done
+    return 0
+}
+
+for i in *
+do
+    if included $i
+    then
+        do_link $i
+    fi
 done
