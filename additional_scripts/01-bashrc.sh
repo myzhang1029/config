e17e0c83f38881ac0d06e2208c270d098e89da52
Zhang Maiyun <myzhang1029@hotmail.com>
2020-09-24 22:12:42 +0800

    Add more to shells

--- additional_scripts/01-bashrc.sh
+++ additional_scripts/01-bashrc.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
-# Invoke .aliases in .bashrc
+# Invoke .aliases in .bashrc .zshrc or others
 
-# Avoid duplicate add
-grep '\. ~/\.aliases' ~/.bashrc >/dev/null || cat <<'_EOS' >>~/.bashrc
-# Added by myzhang1029/config.git
-if [ -f ~/.aliases ]; then
-    . ~/.aliases
+adder()
+{
+    # Avoid duplicate add
+    grep 'ADDED BY MYZHANG1029/CONFIG' "$1" >/dev/null || cat <<'_EOS' >>"$1"
+
+# ADDED BY MYZHANG1029/CONFIG
+if [ -f ~/.shellrc_ext ]; then
+    . ~/.shellrc_ext
 fi
+# END BY MYZHANG1029/CONFIG
 _EOS
+}
+
+for rc in "$HOME/.zshrc" "$HOME/.bashrc"
+do
+    echo "Operating on $rc"
+    [ -e "$rc" ] && adder "$rc"
+done
