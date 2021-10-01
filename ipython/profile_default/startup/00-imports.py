df9b9e85a4181843db648e21afa1e4408a1dbc96
Zhang Maiyun <myzhang1029@hotmail.com>
2021-09-30 22:38:27 -0700

    Update IPython config

--- ipython/profile_default/startup/00-imports.py
+++ ipython/profile_default/startup/00-imports.py
@@ -1,5 +1,18 @@
+import json
 import os
 import sys
-import numpy as np
-import json
+from importlib import import_module as _import
 from math import *
+
+optional_imports = [
+    ("np", "numpy"),
+    ("pd", "pandas"),
+    ("plt", "matplotlib.pyplot"),
+    ("tf", "tensorflow")
+]
+
+for name, module in optional_imports:
+    try:
+        setattr(__builtins__, name, _import(module))
+    except ImportError:
+        pass
--- ipython/profile_default/startup/01-magics.py
+++ ipython/profile_default/startup/01-magics.py
@@ -1,17 +1,24 @@
-from IPython.core.magic import register_line_magic
 import functools
 
+from IPython.core.magic import register_line_magic
+
+
 @register_line_magic
-def getutf(str):
-    for c in str:
-        print(hex(ord(c)))
+def getutf(string):
+    """Get the Unicode index of a character."""
+    for char in string:
+        print(hex(ord(char)))
+
 
 def shell(func):
+    """Make a line magic to call the named shell command."""
     @functools.wraps(func)
     def wrapper(*args, **kw):
+        """Call the named shell command."""
         os.system(f"{func.__name__} {args[0]}")
         return func(*args, **kw)
     return register_line_magic(wrapper)
+
 
 @shell
 def vi(line): pass
@@ -29,5 +36,6 @@ def sudo(line): pass
 def aria2c(line): pass
 @shell
 def brew(line): pass
+
 
 del getutf, vi, gcc, make, cmake, git, sudo, aria2c, brew
